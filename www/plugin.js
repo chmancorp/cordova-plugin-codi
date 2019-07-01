3fabfd6f64daa0a18fa4e2678030523b3fab0de9
Julio Reyes <morris400jul@gmail.com>
2019-07-01 13:38:07 -0500

    upsdate

--- www/plugin.js
+++ www/plugin.js
@@ -4,14 +4,9 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'MiPlugin';
 
 var MiPlugin = {
-  saludo: function (name, successCallback, errorCallback){
-        exec(successCallback, errorCallback, PLUGIN_NAME, "saludar", [name]);
-  },
+  
   echo: function(codr,idh,phone,gId, cb){
     exec(cb, null, PLUGIN_NAME, "echo", [codr,idh,phone,gId,cb]);
-  },
-  test: function(np,cb){
-    exec(cb,null,PLUGIN_NAME,"test",[np]);
   }
 };
 
