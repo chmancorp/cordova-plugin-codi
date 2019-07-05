9d6caf98f59bd4f9d5884a014765445c36982d79
Minimalist <erick@minimalist.mx>
2019-07-05 11:14:46 -0500

    plugin.js resolve

--- www/plugin.js
+++ www/plugin.js
@@ -8,7 +8,7 @@ var MiPlugin = {
   echo: function(codr,idh,phone,gId, cb){
     exec(cb, null, PLUGIN_NAME, "echo", [codr,idh,phone,gId,cb]);
   },
-  getSHA512: function(cadena,success,error){
+  getSHA512: function(cadena,success){
     exec(success,null,PLUGIN_NAME,"getSHA512",[cadena])
   }
 };
